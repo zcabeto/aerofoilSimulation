a1845dcef7c815b7a1ce5397e7fd3457f34db158
zcabeto <zcabeto@ucl.ac.uk>
2022-10-25 13:41:32 +0100

    pre-stall estimation added
    after research added ability to estimate lift with (pre-stall) coefficient of lift based on camber, angle of attack, and wing aspect ratio

--- bernoulliCalculator.py
+++ bernoulliCalculator.py
@@ -1,9 +1,26 @@
 # Bernoulli's Principle Calculator
 
-coefLift = 1
-density = 1.204                     # kg/m3 of air
-velocity = input("Enter velocity: ")# m/s of aerofoil
-area = 1                            # m2 of aerofoil top
+import math
 
-lift = coefLift * (0.5*density*velocity) * area
-print(lift)
+density = 1.204
+area = float(input("Enter area: "))
+AR = float(input("Enter Aspect Ratio of wing: "))
+camber = float(input("Enter camber percentage: "))
+weight = float(input("Enter Plane Weight: "))
+
+print("Plane has taken off...")
+velocity = float(input("Enter velocity: "))
+angle = float(input("Enter angle of attack (degrees): "))
+
+a = angle * math.pi/180         # convert to radians
+p = camber/100
+CLA = (2*math.pi)/(1 + (2/AR))  # coefficients...
+CLZ = math.pi * (4*p - 2 + math.sqrt(16 * (p**2) + 4))
+CL = (a * CLA) + CLZ
+
+dynamicPressure = 0.5*density*(velocity**2)
+lift = round(CL * dynamicPressure * area)
+
+print(("The plane has " + str(lift) + "N of lift. This gives "
+       + str(round(lift-weight)) + "N of upward force.").strip())
+
